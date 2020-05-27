f84f8ea1affebdbd2df875f9c7628feb7253be34
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2020-05-27 18:55:41 +0300

    fix 2

--- modules/search_diameter_in_graph/src/graph_diameter_search_app.cpp
+++ modules/search_diameter_in_graph/src/graph_diameter_search_app.cpp
@@ -20,20 +20,17 @@ int GraphApp::ParseValue(const char* data) {
 }
 
 std::string GraphApp::operator()(int argc, const char** argv) {
-  Arguments args;
-
   if (!validateNumOfArgs(argc, argv)) {
     return message_;
     }
   
   std::ostringstream stream;
-  Graph graph;
   try {
 	  int n = ParseValue(argv[1]);
-	  graph = graph(n);
+	  Graph graph = graph(n);
 	  int edges = argc - 2;
 	  for (int i = 2; i < edges; i++) {
-      graph.addEdge(ParseValue(argv[i], ParseValue(argv[i+1]);
+      graph.addEdge(ParseValue(argv[i], ParseValue(argv[i+1]));
 	  }
 	  stream << graph.diameterSearch();
     } 
@@ -62,7 +59,7 @@ bool GraphApp::validateNumberOfArguments(int argc, const char** argv) {
     help(argv[0]);
     return false;
   } 
-  int count = parseInt(argv[1]);
+  int count = parseValue(argv[1]);
   if (argc > count*(count-1) + 2 || count <= 0 || (argc - 2)%2 != 0) {
     help(argv[0], "Wrong number of arguments.\n\n");
     return false;
