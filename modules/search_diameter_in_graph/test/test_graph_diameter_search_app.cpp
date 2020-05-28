f053d67d3ee3320093ae4a92158d580298bd97d3
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2020-05-28 12:09:30 +0300

    added test

--- modules/search_diameter_in_graph/test/test_graph_diameter_search_app.cpp
+++ modules/search_diameter_in_graph/test/test_graph_diameter_search_app.cpp
@@ -52,16 +52,16 @@ TEST_F(GraphAppTest, Check_Number_Of_Arguments) {
   Assert("Wrong number of arguments");
 }
 
-/*TEST_F(GraphAppTest, Can_Detect_Wrong_Operation_Format) {
+TEST_F(GraphAppTest, Can_Detect_Wrong_Operation_Format) {
   // Arrange
-  std::vector<std::string> args = {"3", "1", "2", ","};
+  std::vector<std::string> args = {"3", "1", "2", "2", ","};
 
   // Act
   Act(args);
 
   // Assert
   Assert("Wrong format");
-}*/
+}
 
 /*TEST_F(GraphAppTest, Can_Get_Diameter_Of_Graph) {
   // Arrange
